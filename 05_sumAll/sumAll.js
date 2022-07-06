d592885c9550663ee4eb4b21212f2f0a25564dfa
mados <dasomee@outlook.com>
2022-07-06 00:44:05 -0400

    Passed sumAll test conditions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    if (firstNum < 0 || lastNum < 0) {
+        return 'ERROR'
+    } else if (typeof(firstNum) != 'number' || typeof(lastNum) != 'number') {
+        return 'ERROR'
+    } else {
+        let totalInt = Math.abs(lastNum - firstNum) + 1;
+        return totalInt * (firstNum + lastNum) / 2;
+    }
 };
 
 // Do not edit below this line
