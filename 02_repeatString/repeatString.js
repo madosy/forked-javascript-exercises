e92a25958a5d2175eef849e3d00308d6bea28d0d
mados <dasomee@outlook.com>
2022-07-05 23:06:57 -0400

    pass all 02_repeatString test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString;
+    (num >= 0)? finalString = '' :
+    finalString = "ERROR";
+    for (let i=0; i < num; i++){
+        finalString = finalString.concat(string);
+    }
+    return finalString
 };
 
 // Do not edit below this line
