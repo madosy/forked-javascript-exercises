57cfa33cc6951197777485cd035e2edf899531c3
mados <dasomee@outlook.com>
2022-07-06 00:29:55 -0400

    Pass removeFromArray test conditions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...itemsForRemoval) {
+    for (const itemRemoved of itemsForRemoval) {
+        let word = itemRemoved;
+        inputArray.forEach(spliceBadItem)
 
-};
+        function spliceBadItem (arrItem, index, arr) {
+            if (arrItem === itemRemoved) {
+                arr.splice(index,1)
+            }
+        }
+    }
+    return inputArray
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
