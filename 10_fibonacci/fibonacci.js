0d5b0f7a0b7771d41bfc1de13bcc82e3846eed1c
madosy <dasomee@outlook.com>
2022-08-04 13:01:01 -0400

    Fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let n = parseInt(input)
+    if (n<0) return "OOPS"
 
+    if (n>10){
+        return Math.floor(((1.618034**n) - ( (1-1.618034)**n)) / Math.sqrt(5))
+    }
+
+    let fibArr = [0,1,1]
+    if (n<3) return fibArr[n]
+
+    fibArr[n] = fibonacci(n-1) + fibonacci(n-2)
+    return fibArr[n] 
 };
 
 // Do not edit below this line
