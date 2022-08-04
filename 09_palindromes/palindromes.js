02a25c8229a7d43ba0325c76d284760e658b56f0
madosy <dasomee@outlook.com>
2022-08-04 01:54:06 -0400

    Palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function (myStr) {
+    //convert all to lowercase
+    let myArr = Array.from(myStr.toLowerCase());
+
+    //remove items that are not alpha
+    const onlyLetters = (str) => /^[a-zA-Z]+$/.test(str);
+    let filteredArr = myArr.filter(onlyLetters)
+    //check true until false
+
+    arrLen = filteredArr.length;
+
+    for (let i = 0; i < arrLen; i++) {
+        firstItem = filteredArr[i];
+        secondItem = filteredArr[arrLen-i-1];
+        if (firstItem !== secondItem) {
+            return false
+        }
+    } return true
+    
+    
 
 };
 
