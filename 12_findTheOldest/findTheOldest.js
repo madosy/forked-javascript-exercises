8a6390a006a9320eb1ff0a8bacc29c4674f9b0d6
madosy <dasomee@outlook.com>
2022-08-04 13:57:45 -0400

    findTheOldest complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce( (previousPerson, currentPerson) => {
+
+        if (!Object.hasOwn(previousPerson,'yearOfDeath')) previousPerson.yearOfDeath = (new Date()).getFullYear()
+        if (!Object.hasOwn(currentPerson,'yearOfDeath')) currentPerson.yearOfDeath = (new Date()).getFullYear()
+    
+        previousAge = previousPerson.yearOfDeath - previousPerson.yearOfBirth
+        currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth
+        
+        if (currentAge > previousAge) {
+            return currentPerson 
+        } else return previousPerson
+    })
 
 };
 
